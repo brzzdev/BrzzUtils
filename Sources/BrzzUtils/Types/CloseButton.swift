4d5f4a7232c98062e14e2fa4c4f54c0f38c6e94b
Paul <paul@brzz.dev>
2024-10-08 17:00:56 +0100

    Add CloseButton

---
+++ Sources/BrzzUtils/Types/CloseButton.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+public struct CloseButton: View {
+	public enum Style {
+		case circle
+		case text
+	}
+	
+	@Environment(\.dismiss)
+	private var dismiss
+	
+	private let style: Style
+	
+	public var body: some View {
+		Button(
+			action: {
+				dismiss()
+			},
+			label: {
+				switch style {
+				case .circle:
+					Image(systemName: "xmark.circle.fill")
+						.font(.system(size: 20))
+						.tint(.primary)
+					
+				case .text:
+					Text("Close")
+				}
+			}
+		)
+		.keyboardShortcut(.cancelAction)
+		.accessibilityLabel(Text("Close"))
+	}
+	
+	public init(style: Style = .circle) {
+		self.style = style
+	}
+}
