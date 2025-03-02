5104828b30419c0a484a17028bfd5dc7e6d786c5
Paul <paul@brzz.dev>
2025-03-02 23:51:36 +0000

    Add isLoading property

--- Sources/BrzzUtils/Types/LoadingState.swift
+++ Sources/BrzzUtils/Types/LoadingState.swift
@@ -5,6 +5,15 @@ public enum LoadingState: Equatable, Sendable {
 	case loaded
 	case refreshing
 
+	public var isLoading: Bool {
+		switch self {
+		case .firstLoad, .refreshing:
+			return true
+		case .loaded:
+			return false
+		}
+	}
+
 	public mutating func setLoading(_ loading: Bool) {
 		if loading {
 			guard self != .firstLoad else { return }
