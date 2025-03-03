b79138963866ecc8aace1319fa875e28ce1d4bf3
Paul <paul@brzz.dev>
2025-03-03 01:50:39 +0000

    Add random Strings

---
+++ Sources/BrzzUtils/Extensions/Ext+String.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+extension String {
+	public static func random(
+		length: Int = Int.random(in: 1 ... 20)
+	) -> String {
+		let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		return String((0 ..< length).map { _ in letters.randomElement()! })
+	}
+}
+
+extension [String] {
+	public static func random(
+		count: Int
+	) -> [String] {
+		return (0 ..< count).map { _ in
+			String.random()
+		}
+	}
+}
