1090d9a28b4437965eef46b367dfe2624ccdc1b4
Paul <paul@brzz.dev>
2025-03-03 16:46:37 +0000

    Fix availability

--- Sources/BrzzUtils/Extensions/Ext+UncheckedSendable.swift
+++ Sources/BrzzUtils/Extensions/Ext+UncheckedSendable.swift
@@ -3,7 +3,7 @@ import Combine
 extension AnyPublisher: @retroactive @unchecked Sendable where Output: Sendable,
 	Failure: Sendable {}
 
-@available(iOS 18.0, *)
+@available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
 extension AsyncPublisher: @retroactive @unchecked Sendable where P: Sendable, Element: Sendable,
 	Failure: Sendable {}
 
--- Sources/BrzzUtils/Extensions/Ext+View.swift
+++ Sources/BrzzUtils/Extensions/Ext+View.swift
@@ -1,13 +1,11 @@
 import SwiftUI
 
-@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 extension View {
 	public func onFirstAppear(perform action: @escaping () async -> Void) -> some View {
 		modifier(FirstAppearModifier(action: action))
 	}
 }
 
-@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 private struct FirstAppearModifier: ViewModifier {
 	let action: () async -> Void
 	@State
