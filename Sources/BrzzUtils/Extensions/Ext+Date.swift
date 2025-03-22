c0a9f0542fa4c2daa4183c0b7ac4523fbc00d173
Paul <paul@brzz.dev>
2025-03-22 13:58:04 +0000

    Create BrzzDateStyle

--- Sources/BrzzUtils/Extensions/Ext+Date.swift
+++ Sources/BrzzUtils/Extensions/Ext+Date.swift
@@ -2,43 +2,49 @@ import Dependencies
 import Foundation
 
 extension Date {
-	/// Returns an ISO 8601 formatted string representation of the date
-	///
-	/// This method creates a string with full date, time with colons,
-	/// fractional seconds, and a space between date and time components.
-	/// The string is formatted using the current time zone.
-	///
-	/// - Returns: A string like "1970-01-01 00:00:00.000" in the current time zone
-	public var isoString: String {
-		let formatter = ISO8601DateFormatter()
-		formatter.formatOptions = [
-			.withFullDate,
-			.withSpaceBetweenDateAndTime,
-			.withTime,
-			.withColonSeparatorInTime,
-			.withFractionalSeconds,
-		]
-		formatter.timeZone = TimeZone.current
-		return formatter.string(from: self)
+	public enum BrzzDateStyle {
+		/// Returns a localized string showing only the day and month
+		/// E.g - "16/03" for UK locale
+		case dayMonth
+		/// Returns a localized string showing the day, month, and time
+		/// E.g - "16/03 13:00" for UK locale
+		case dayMonthTime
+		/// Returns an ISO 8601 formatted string representation of the date
+		/// E.g - "1970-01-01 00:00:00.000"
+		case iso
 	}
 
-	/// Returns a localized string showing only the day and month
-	///
-	/// This property creates a representation of the date showing only the day
-	/// and month in a format appropriate for the user's current locale.
-	///
-	/// - Returns: A localized string representing the day and month (e.g., "16/03" for UK locale)
-	public var localizedDayMonthString: String {
-		let formatter = DateFormatter()
-		@Dependency(\.locale)
-		var locale
+	/// Converts `self` to its textual representation that contains both the date and time parts. The exact format depends on the user's preferences.
+	/// - Parameters:
+	///   - style: The custom style used.
+	/// - Returns: A `String` describing `self`.
+	public func formatted(
+		style: BrzzDateStyle
+	) -> String {
+		switch style {
+		case .dayMonth:
+			let formatter = DateFormatter()
+			formatter.setLocalizedDateFormatFromTemplate("dd/MM")
+			return formatter.string(from: self)
 
-		formatter.dateFormat = DateFormatter.dateFormat(
-			fromTemplate: "dM",
-			options: 0,
-			locale: locale
-		) ?? "dd/MM"
+		case .dayMonthTime:
+			let formatter = DateFormatter()
+			formatter.setLocalizedDateFormatFromTemplate("dd/MM HH:mm")
+			return formatter.string(from: self)
 
-		return formatter.string(from: self)
+		case .iso:
+			let formatter = ISO8601DateFormatter()
+			formatter.formatOptions = [
+				.withFullDate,
+				.withSpaceBetweenDateAndTime,
+				.withTime,
+				.withColonSeparatorInTime,
+				.withFractionalSeconds,
+			]
+			@Dependency(\.timeZone)
+			var timeZone
+			formatter.timeZone = timeZone
+			return formatter.string(from: self)
+		}
 	}
 }
