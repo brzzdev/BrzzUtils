2fb5c07270389363db4d3c9e0f2588c20aa302cb
Paul <paul@brzz.dev>
2025-07-23 12:53:31 +0100

    feature: data prettyPrinted

--- Sources/BrzzUtils/Extensions/Ext+Data.swift
+++ Sources/BrzzUtils/Extensions/Ext+Data.swift
@@ -1,6 +1,17 @@
 import Foundation
 
 extension Data {
+	public var prettyPrinted: String {
+		guard
+			let json = try? JSONSerialization.jsonObject(with: self, options: []),
+			let data = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted),
+			let jsonString = String(data: data, encoding: .utf8) else {
+			return "Invalid JSON"
+		}
+
+		return jsonString
+	}
+
 	public var utf8String: String {
 		guard let string = String(data: self, encoding: .utf8) else {
 			assertionFailure("String not correctly decoding from Data")
